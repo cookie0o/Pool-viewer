53929432be215474a8d2ea5f5395b6904bea2fef
Raphael <raphael.contact@zohomail.eu>
2025-04-24 16:32:46 +0200

    added cards on home for later use

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,12 @@
 </script>
   
 <LayoutShell title="Home">
-    <p>Home page content here...</p>
+  <div style="display: flex; justify-content: space-between; gap: 16px; width: 100%; height: 150px">
+      <div class="card preset-filled-surface-100-900 p-4 text-center" style="flex-grow: 1;">
+          <p>Card</p>
+      </div>
+      <div class="card preset-filled-surface-100-900 p-4 text-center" style="flex-grow: 1;">
+          <p>Card</p>
+      </div>
+  </div>
 </LayoutShell>
