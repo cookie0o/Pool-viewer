20a819fe9d1dcf9286cfb5d0afc4b1844394979c
Raphael <raphael.contact@zohomail.eu>
2025-04-25 22:05:33 +0200

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,10 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
     kit: {
         adapter: adapter(),
-        paths: {
-            base: process.argv.includes('dev') ? '' : "/Pool-viewer"
-        }
-    }
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
