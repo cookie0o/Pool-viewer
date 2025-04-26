b223b0c75b67765cd1b16a558f969cf085d3bfa7
Raphael <raphael.contact@zohomail.eu>
2025-04-26 12:19:26 +0200

    possible fix

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,12 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        appDir: 'web',
-        adapter: adapter(),
-        paths: {
-            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-        }
-    },
-    preprocess: vitePreprocess()
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
+
 export default config;
