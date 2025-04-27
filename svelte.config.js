4f267ac212ee8e51f6dadfa12f3c88e4068fb2bb
Raphael <raphael.contact@zohomail.eu>
2025-04-27 16:07:05 +0200

    fixed css not loading on static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from "@sveltejs/adapter-static";
+import adapterStatic from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapterStatic(),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
